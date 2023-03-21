ff95032a63c665c0e3c85a55fdeb56e7665ac6ec
Vladimir Alekseev <mail@vlal.ru>
2023-03-21 12:37:37 +0300

    fix exported type

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import Map from './Map';
+import type { RegionObj } from './Map.d';
 import { getLatLngToMap } from './utils';
 
+export type { RegionObj };
 export { getLatLngToMap, Map };
 export default Map;
